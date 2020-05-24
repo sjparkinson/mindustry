260c20d141009378990e6f99479980eebd8ce3a1
Samuel Parkinson <sam.james.parkinson@gmail.com>
2020-05-24 10:16:08 +0100

    Fix the start commands

--- start-mindustry.sh
+++ start-mindustry.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-SERVER_NAME:=WFH Covid-19
-PLAYER_LIMIT:=8
+SERVER_NAME=WFH Covid-19
+PLAYER_LIMIT=8
 
 java -jar /opt/mindustry/server-release.jar \
-    "host,playerLimit $PLAYER_LIMIT,name $SERVER_NAME"
+    "host,playerLimit $PLAYER_LIMIT,config name $SERVER_NAME"
