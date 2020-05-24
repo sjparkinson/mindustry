43949e403860c93c9f07a8a7599c1f2f80d90402
Samuel Parkinson <sam.james.parkinson@gmail.com>
2020-05-24 10:52:07 +0100

    playerLimit becomes playerlimit

--- start-mindustry.sh
+++ start-mindustry.sh
@@ -4,4 +4,4 @@ SERVER_NAME=WFH Covid-19
 PLAYER_LIMIT=8
 
 java -jar /opt/mindustry/server-release.jar \
-    "host,config name $SERVER_NAME,playerLimit $PLAYER_LIMIT,help"
+    "config name $SERVER_NAME,playerlimit $PLAYER_LIMIT,host"
